0004782f8280034a0a302479aea613bcee1ca30d
michal.niegrzybowski@gmail.com <michal.niegrzybowski@gmail.com>
2017-05-14 23:13:14 +0200

    remove console.writeline

--- src/Recommender/App.fs
+++ src/Recommender/App.fs
@@ -10,7 +10,6 @@ namespace Recommender
                 (parameters :?> Nancy.DynamicDictionary).[param].ToString()
         do
             this.Get.["/Recommend/{userId}/{limit}"] <- fun parameters ->
-                System.Console.WriteLine(sprintf "%A, %A" parameters?userId parameters?limit)
                 let userId = parameters?userId |> int64
                 let limit = parameters?limit |> int
                 this.Response.AsJson(_recommender.Recommend(userId, limit)) :> obj
